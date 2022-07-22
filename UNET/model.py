916750c5295b96485f716df7b72c2d35d14dd8df
Chou50 <107591950+Chou50@users.noreply.github.com>
2022-07-22 15:03:43 +0900

    Update model.py

--- UNET/model.py
+++ UNET/model.py
@@ -2,6 +2,10 @@ import torch
 import torch.nn as nn
 from torchvision import models
 
+'''
+define the UNET model. ResNet18 is used as encoder here. 
+'''
+
 
 def convrelu(in_channels, out_channels, kernel, padding):
     return nn.Sequential(
