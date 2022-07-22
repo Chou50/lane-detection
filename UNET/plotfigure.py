98cc5dc052d351171ed689c57a2600f63bff6ae8
Chou50 <107591950+Chou50@users.noreply.github.com>
2022-07-22 15:09:26 +0900

    Update plotfigure.py

--- UNET/plotfigure.py
+++ UNET/plotfigure.py
@@ -1,9 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+'''
+define the function of plotting figure.
+Before plotting, images data need to be transformed back to the range of (0, 255). 
+It is reverse transformation of normalization.
+'''
 
 def reverse_transform(inp):
-    # after ToTensor, figure shape is (H, W, 3), need to be transposed to (3, H, W) to be plotted
+    # after ToTensor, figure shape is (H, W, 3), so need to be transposed to (3, H, W) to be plotted
     inp = inp.numpy().transpose((1, 2, 0))
     mean = np.array([0.485, 0.456, 0.406])
     std = np.array([0.229, 0.224, 0.225])
@@ -19,6 +24,7 @@ def plot_figure(image, mask):
     plt.subplot(1, 2, 1)
     plt.imshow(img)
 
+    # get the index of (value == 1) on mask
     mask = mask.numpy()
     mask = (mask != 0).astype(np.uint8)
     mask_idx = np.where(mask[:, :, 0])
